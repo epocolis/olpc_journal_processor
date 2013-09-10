c5fcf1d91cc51476275d2c089e1163ea2050d9c3
Philip Withnall <philip@tecnocode.co.uk>
2013-09-10 13:28:34 -0600

    Add distutils and RPM support
    This allows the project to be packaged by running:
     • ./setup.py sdist
     • ./setup.py bdist
     • ./setup.py bdist_rpm

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# coding=utf-8
+# vim: ai ts=4 sts=4 et sw=4 ft=python
+
+from distutils.core import setup
+from subprocess import check_output
+
+# Get the git version from `git describe`
+git_version = check_output(['git', 'describe',
+                            '--long', '--tags', '--dirty', '--always'])
+git_version = git_version.rstrip('\n')
+
+setup(name='olpc-journal-processor',
+      description='Script to extract data from the OLPC Journal data store.',
+      version=git_version,
+      author='Leotis Buchanan, Philip Withnall',
+      author_email='philip@tecnocode.co.uk',
+      license='GPLv3',
+      platforms=['noarch'],
+      url='https://github.com/Leotis/olpc_journal_processor',
+      scripts=['olpc-journal-processor'])
