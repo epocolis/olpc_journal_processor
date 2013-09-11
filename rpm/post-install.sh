4f438dfac44edf0cdf4511e79343275dca398848
Philip Withnall <philip@tecnocode.co.uk>
2013-09-11 14:40:59 -0600

    Allow parameters to be passed to the forced SSH command
    This allows invocation to output general statistics, or URI statistics, for
    example:
    	ssh olpcjournalprocessor@my-server \
    		olpc-journal-processor -o - -u /dev/null
    vs.
    	ssh olpcjournalprocessor@my-server \
    		olpc-journal-processor -o /dev/null -u -

--- rpm/post-install.sh
+++ rpm/post-install.sh
@@ -11,7 +11,7 @@ fi
 # Generate an SSH key for the user.
 ssh-keygen -C "olpcjournalprocessor remote access" -t rsa -f "%{_datadir}/%{name}/id_rsa" -N ""
 
-echo -n "command=\"%{_bindir}/olpc-journal-processor\",no-port-forwarding,no-X11-forwarding,no-pty " >> "%{_datadir}/%{name}/.ssh/authorized_keys"
+echo -n "command=\"%{_bindir}/olpc-journal-processor ${SSH_ORIGINAL_COMMAND#* }\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty " >> "%{_datadir}/%{name}/.ssh/authorized_keys"
 cat "%{_datadir}/%{name}/id_rsa.pub" >> "%{_datadir}/%{name}/.ssh/authorized_keys"
 
 chmod 600 "%{_datadir}/%{name}/.ssh/authorized_keys" "%{_datadir}/%{name}/id_rsa" "%{_datadir}/%{name}/id_rsa.pub"
